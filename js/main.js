9f96574474e12c832ba72a61b414da22c580d77c
SaadHussein <sa3dhusseein@gmail.com>
2023-10-01 01:35:37 +0300

    Finish Simple Project with Bootstrap

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const sidebarId = document.getElementById("sidebarId");
+
+if (localStorage.getItem("isSmall") === "yes") {
+  sidebarId.classList.add("small-sidebar");
+} else {
+  sidebarId.classList.remove("small-sidebar");
+}
+const toggleSidebar = () => {
+  if (localStorage.getItem("isSmall") === "yes") {
+    localStorage.setItem("isSmall", "no");
+    sidebarId.classList.remove("small-sidebar");
+  } else {
+    localStorage.setItem("isSmall", "yes");
+    sidebarId.classList.add("small-sidebar");
+  }
+  console.log("Hi");
+};
